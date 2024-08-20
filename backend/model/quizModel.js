d25a914ee5536430fb62b6f2dcaa738ab9cbe66b
VasaR <vasanthias52@gmail.com>
2024-08-20 17:54:26 +0530

    model done

---
+++ backend/model/quizModel.js
@@ -0,0 +1,82 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const quizSchema = new Schema({
+  quizName: {
+    type: String,
+    required: true,
+  },
+  quizType: {
+    type: String,
+    enum: ["qNa", "poll"],
+    required: true,
+  },
+  impressions: {
+    type: Number,
+    default: 0,
+  },
+  createdBy: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdTime: {
+    type: String,
+    required: true,
+  },
+  quizQuestions: [
+    {
+      _id: false,
+      questionNo: {
+        type: Number,
+        required: true,
+        unique: false,
+      },
+      question: {
+        type: String,
+        required: true,
+      },
+      optionType: {
+        type: String,
+        enum: ["text", "image", "textNimage"],
+        required: true,
+      },
+      options: {
+        type: [String],
+        required: true,
+      },
+      correctAnswer: {
+        type: Number,
+        required: true,
+      },
+      timer: {
+        type: Number,
+        default: null,
+      },
+      analytics: {
+        _id: false,
+        peopleAttempted: {
+          type: Number,
+          default: 0,
+        },
+        answeredCorrectly: {
+          type: Number,
+          default: 0,
+        },
+        answeredIncorrectly: {
+          type: Number,
+          default: 0,
+        },
+        optionChoosed: {
+          type: [Number],
+          default: [],
+        },
+      },
+    },
+  ],
+});
+
+const Quiz = mongoose.model("Quiz", quizSchema);
+
+module.exports = Quiz;

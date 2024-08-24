5c487369ac6c4464a1a6fec98e9edea033995706
VasaR <vasanthias52@gmail.com>
2024-08-24 23:08:38 +0530

    validation for quiz done

---
+++ backend/middleware/validateNewQuiz.js
@@ -0,0 +1,39 @@
+const validateNewQuiz = (req, res, next) => {
+  const { quizName, quizType, quizQuestions } = req.body;
+  try {
+    if (!quizName || !quizType || !quizQuestions) {
+      return res.status(400).json({
+        success: false,
+        message: "Please provide all the required fields",
+      });
+    }
+
+    for (const question of quizQuestions) {
+      if (
+        !question.questionNo ||
+        !question.question ||
+        !question.optionType ||
+        !question.options ||
+        question.options.length === 0 ||
+        (question.correctAnswer !== null &&
+          typeof question.correctAnswer !== "number")
+      ) {
+        return res.status(400).json({
+          success: false,
+          message:
+            "Please provide all the required fields for each question here",
+        });
+      }
+    }
+
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      success: false,
+      message: "Server error",
+    });
+  }
+};
+
+module.exports = validateNewQuiz;

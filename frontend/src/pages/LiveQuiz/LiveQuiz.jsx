6cd5c5694f127b00db7062f160825a771e07f7ec
VasaR <vasanthias52@gmail.com>
2024-08-27 04:25:41 +0530

    live quiz page over

--- frontend/src/pages/LiveQuiz/LiveQuiz.jsx
+++ frontend/src/pages/LiveQuiz/LiveQuiz.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import styles from "./LiveQuiz.module.css";
 import Quiz from "../../components/LiveQuizComponents/Quiz/Quiz";
-import QnaEnd from "../../components/LiveQuizComponents/QnaEnd/QnaEnd";
-import PollEnd from "../../components/LiveQuizComponents/PollEnd/PollEnd";
+import { Routes, Route } from "react-router-dom";
+import ErrorPage from "../ErrorPage/ErrorPage";
 
 const LiveQuiz = () => {
   return (
     <div className={styles.quizWrapper}>
-      {/* <Quiz /> */}
-      {/* <QnaEnd /> */}
-      <PollEnd />
+      <Routes>
+        <Route path="/:id" element={<Quiz />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
     </div>
   );
 };

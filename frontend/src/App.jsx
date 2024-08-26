7834725778a579985942be3f4160b383d35eb975
VasaR <vasanthias52@gmail.com>
2024-08-27 04:27:13 +0530

    feat: ready for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import AuthPage from "./pages/AuthPage/AuthPage";
-import DashboardPage from "./pages/DashboardPage/DashboardPage";
 import LiveQuiz from "./pages/LiveQuiz/LiveQuiz";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
+import LazyLoader from "./components/LoadinComponents/LazyLoader/LazyLoader";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
+import { useAuth } from "./context/AuthContext";
+const DashboardPage = React.lazy(() =>
+  import("./pages/DashboardPage/DashboardPage")
+);
 
 const App = () => {
+  const { token } = useAuth();
   return (
     <>
       <ToastContainer />
-      {/* <AuthPage /> */}
-      {/* <DashboardPage /> */}
-      <LiveQuiz />
+      <Routes>
+        <Route
+          path="/"
+          element={token ? <Navigate to={"/main/dashboard"} /> : <AuthPage />}
+        />
+        <Route element={<ProtectedRoute />}>
+          <Route
+            path="/main/*"
+            element={
+              <Suspense fallback={<LazyLoader />}>
+                <DashboardPage />
+              </Suspense>
+            }
+          />
+        </Route>
+        <Route path="live/*" element={<LiveQuiz />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
     </>
   );
 };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,17 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import AuthProvider from "./context/AuthContext.jsx";
+import { Provider } from "react-redux";
+import appStore from "./redux/store/appStore.js";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <BrowserRouter>
+    <Provider store={appStore}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Provider>
+  </BrowserRouter>
 );

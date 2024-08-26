1c3b68d06f758883fd34dc72e082ef9df2be7b2c
VasaR <vasanthias52@gmail.com>
2024-08-27 02:16:53 +0530

    protected route done only for main path

--- frontend/src/components/ProtectedRoute/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,8 +1,16 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
+import { useEffect } from "react";
 
 const ProtectedRoute = () => {
-  const { token } = useAuth();
+  const { token, logoutContext } = useAuth();
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token");
+    if (!storedToken) {
+      logoutContext();
+    }
+  }, [token]);
 
   return token ? <Outlet /> : <Navigate to="/" />;
 };
